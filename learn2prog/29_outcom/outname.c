3a25dd23923890080ff2501a590f44ef77021696
sbwcwso <lijunjie199502@gmail.com>
2024-03-06 16:01:43 +0800

    finish 29_outcom

--- learn2prog/29_outcom/outname.c
+++ learn2prog/29_outcom/outname.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include "outname.h"
 
+#define FILE_EXTENSION ".counts"
+
 char * computeOutputFileName(const char * inputName) {
-  //WRITE ME
+  char *outputName = malloc((strlen(inputName) + strlen(FILE_EXTENSION) + 1) * sizeof(*outputName));
+  if (outputName == NULL) {
+    perror("malloc failed");
+  }
+  strcpy(outputName, inputName);
+  strcat(outputName, FILE_EXTENSION);
+  return outputName;
 }
